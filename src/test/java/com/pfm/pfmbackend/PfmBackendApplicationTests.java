8b2e0a97c6ecdf1f4684f32454658193dccd3df6
AbdelMouhaimenDakhlia <abdelmouhaimen.dakhlia@esprit.tn>
2025-02-15 15:01:09 +0100

    Initial commit

---
+++ src/test/java/com/pfm/pfmbackend/PfmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfm.pfmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
